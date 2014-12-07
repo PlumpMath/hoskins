91a34a945b476ca886d19e57b7d4d41f394994d4
Neel Upadhyaya <bahulneel@gmail.com>
2014-12-07 23:18:37 +0000

    Simple pub sub

--- src/hoskins/exchange.clj
+++ src/hoskins/exchange.clj
@@ -1,11 +1,22 @@
-(ns hoskins.exchange)
+(ns hoskins.exchange
+  (:require [clojure.core.async :refer [chan] :as async]))
 
 (def empty-exchange {})
 
 (defn get-ex
   [ex name]
-  (get ex name))
+  (get-in ex [name :chan]))
 
 (defn set-ex
   [ex name c]
-  (assoc ex name c))
+  (assoc-in ex [name :chan] c))
+
+(defn subscribe
+  [ex name topic]
+  (let [c-info (get ex name)
+        loc (if-let [loc (:loc c-info)]
+              loc
+              (async/pub (:chan c-info) :topic))
+        c (chan)]
+    (async/sub loc topic c)
+    [ex c]))
--- test/hoskins/exchange_test.clj
+++ test/hoskins/exchange_test.clj
@@ -1,7 +1,7 @@
 (ns hoskins.exchange-test
   (:require [hoskins.exchange :refer :all]
             [midje.sweet :refer :all]
-            [clojure.core.async :refer [chan]]))
+            [clojure.core.async :refer [chan put! <!!]]))
 
 (unfinished)
 
@@ -9,5 +9,26 @@
        (fact "calling get-ex on an exchange returns nil"
              (get-ex empty-exchange :channel) => nil)
        (fact "setting a channel means you can get it"
-             (let [c (chan)]
-               (-> empty-exchange (set-ex :channel c) (get-ex :channel)) => c)))
+             (let [c1 (chan)
+                   c2 (chan)]
+               (-> empty-exchange (set-ex :channel1 c1) (get-ex :channel1)) => c1
+               (-> empty-exchange (set-ex :channel2 c2) (get-ex :channel2)) => c2)))
+
+(facts "about subscringing to a channel"
+       (let [c (chan)
+             ex (set-ex empty-exchange :channel c)
+             [ex sub-c1] (subscribe ex :channel :topic1)
+             [ex sub-c2] (subscribe ex  :channel :topic2)]
+         (fact "all channels are unique"
+               sub-c1 =not=> c
+               sub-c2 =not=> c
+               sub-c1 =not=> sub-c2)
+         (fact "putting to a channel exits to the sub channels"
+               (let [msg1 {:topic :topic1}
+                     msg2 {:topic :topic2}]
+                 (put! c msg1)
+                 (put! c msg2)
+                 (fact "msg1 arrives on c1"
+                       (<!! sub-c1) => msg1)
+                 (fact "msg2 arrives on c2"
+                       (<!! sub-c2) => msg2)))))
