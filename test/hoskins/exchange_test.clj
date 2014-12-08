cedb0e0af1791c21b5d63aadf6fe251f9ca43ed7
Neel Upadhyaya <bahulneel@gmail.com>
2014-12-08 20:46:53 +0000

    tests for simple local exchange

--- test/hoskins/exchange_test.clj
+++ test/hoskins/exchange_test.clj
@@ -1,7 +1,7 @@
 (ns hoskins.exchange-test
   (:require [hoskins.exchange :refer :all]
             [midje.sweet :refer :all]
-            [clojure.core.async :refer [chan put! <!!]]))
+            [clojure.core.async :refer [chan put! <!!] :as async]))
 
 (unfinished)
 
@@ -14,7 +14,7 @@
                (-> empty-exchange (set-ex :channel1 c1) (get-ex :channel1)) => c1
                (-> empty-exchange (set-ex :channel2 c2) (get-ex :channel2)) => c2)))
 
-(facts "about subscringing to a channel"
+(facts "about subscribing to a channel"
        (let [c (chan)
              ex (set-ex empty-exchange :channel c)
              [ex sub-c1] (subscribe-ex ex :channel :topic1)
@@ -32,3 +32,22 @@
                        (<!! sub-c1) => msg1)
                  (fact "msg2 arrives on c2"
                        (<!! sub-c2) => msg2)))))
+
+(facts "about a local exchange"
+       (let [ex (local-exchange)
+             c (lookup ex :channel :chan)]
+         (fact "lookup returns the same channel"
+               (lookup ex :channel :chan) => c)
+         (fact "Can put and get to the channel"
+               (let [msg :message]
+                 (put! c msg)
+                 (<!! c) => msg))
+         (fact "can subscribe to the channel"
+               (let [msg1 {:topic :topic1 :message "Hello world"}
+                     msg2 {:topic :topic2 :message "Hello world 2"}
+                     sub-c1 (subscribe ex :channel :topic1)
+                     sub-c2 (subscribe ex :channel :topic2)]
+                 (put! c msg1)
+                 (put! c msg2)
+                 (<!! sub-c1) => msg1
+                 (<!! sub-c2) => msg2))))
